e075fbba4f808a6db89302bcc034a351ba670794
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-08-12 00:34:09 -0400

    WIP

--- scripts/spCoinSetup.sh
+++ scripts/spCoinSetup.sh
@@ -1,6 +1,7 @@
 # NOTE: Requires git setup
 
 currDir=$PWD
+# repo=spCoinHardHatTests
 repo=spCoinUniSwapTests
 git clone --recurse-submodules git@github.com:sponsorCoinAdmin/$repo.git
 cd ./$repo/setupEnv
